875248baea8faca577c43484c65de1c9cadc3e71
AdriCer <adrcercar@alu.edu.gva.es>
2025-01-27 08:13:48 +0100

    Create Rectangulo.java

---
+++ Formas.docs/Rectangulo.java
@@ -0,0 +1,19 @@
+package Tarea3Ejercicio4;
+
+public class Rectangulo extends Forma{
+
+    protected double longitud;
+    protected double ancho;
+
+    public Rectangulo(String nombre, double longitud, double ancho) {
+        super(nombre);
+        this.longitud = longitud;
+        this.ancho = ancho;
+    }
+
+    @Override
+    public double Area(){
+        return ancho * longitud;
+    }
+
+}
