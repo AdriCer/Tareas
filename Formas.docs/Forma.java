bbdb940a1ad097fffd5155bb729b1bd34f8b8711
AdriCer <adrcercar@alu.edu.gva.es>
2025-01-27 08:12:52 +0100

    Create Forma.java

---
+++ Formas.docs/Forma.java
@@ -0,0 +1,21 @@
+package Tarea3Ejercicio4;
+
+public class Forma {
+    protected String nombre;
+
+    public Forma(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+    public double Area(){
+
+        return 0;
+
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+}
