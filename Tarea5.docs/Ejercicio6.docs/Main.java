10010a196a8122238949310face7ae6d7b53411d
AdriCer <adrcercar@alu.edu.gva.es>
2025-02-04 08:26:55 +0100

    Create Main.java

---
+++ Tarea5.docs/Ejercicio6.docs/Main.java
@@ -0,0 +1,29 @@
+package Tarea5.Ejercicio6;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Rectangulo rec1 = new Rectangulo(2,20.0,30.0);
+        Rectangulo rec2 = new Rectangulo(2,25.0,50.0);
+        Rectangulo rec3 = new Rectangulo(2,10.0,45.0);
+        Rectangulo rec4 = new Rectangulo(2,30.0,35.0);
+        Rectangulo rec5 = new Rectangulo(2,5.0,10.0);
+        Rectangulo rec6 = new Rectangulo(2,2.0,7.5);
+        Rectangulo rec7 = new Rectangulo(2,200.0,300.0);
+        Rectangulo rec8 = new Rectangulo(2,40.0,15.0);
+        Rectangulo rec9 = new Rectangulo(2,75.0,100.0);
+        Rectangulo rec10 = new Rectangulo(2,80.0,150.0);
+
+        System.out.println(rec1);
+        System.out.println(rec2);
+        System.out.println(rec3);
+        System.out.println(rec4);
+        System.out.println(rec5);
+        System.out.println(rec6);
+        System.out.println(rec7);
+        System.out.println(rec8);
+        System.out.println(rec9);
+        System.out.println(rec10);
+
+    }
+}
