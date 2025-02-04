640f5bcdfba601a2076bde611b64b98ff2febd60
AdriCer <adrcercar@alu.edu.gva.es>
2025-02-04 08:28:33 +0100

    Create Main.java

---
+++ Tarea5.docs/Ejercicio7.docs/Main.java
@@ -0,0 +1,21 @@
+package Tarea5.Ejercicio7;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Estudiante estudainte1 = new Estudiante("Juan",18,175);
+        Estudiante estudainte2 = new Estudiante("Ana",15,165);
+        Estudiante estudainte3 = new Estudiante("Pepe",50,180);
+        Estudiante estudainte4 = new Estudiante("Antonio",25,170);
+        Estudiante estudainte5 = new Estudiante("María",30,160);
+
+        System.out.println("Estudiantes sin ordenar: ");
+        System.out.println(estudainte1);
+        System.out.println(estudainte2);
+        System.out.println(estudainte3);
+        System.out.println(estudainte4);
+        System.out.println(estudainte5);
+
+
+    }
+}
